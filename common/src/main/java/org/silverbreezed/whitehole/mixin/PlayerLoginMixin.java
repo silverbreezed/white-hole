d1eb1c4b01eb10d46498b4930c6deed8ac745728
amirh-works <amirh.forworks@gmail.com>
2026-08-09 20:56:20 +0700

    Improve I/O background task asynchronously

---
+++ common/src/main/java/org/silverbreezed/whitehole/mixin/PlayerLoginMixin.java
@@ -0,0 +1,24 @@
+package org.silverbreezed.whitehole.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
+import net.minecraft.server.players.PlayerList;
+import org.silverbreezed.whitehole.event.VoidDeathHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerList.class)
+public class PlayerLoginMixin {
+    @Inject(method = "placeNewPlayer", at = @At("TAIL"))
+    private void onPlayerJoin(Connection connection, ServerPlayer player, CommonListenerCookie cookie, CallbackInfo ci) {
+        VoidDeathHandler.loadPlayerDataAsync(player);
+    }
+
+    @Inject(method = "remove", at = @At("HEAD"))
+    private void onPlayerLeave(ServerPlayer player, CallbackInfo ci) {
+        VoidDeathHandler.unloadPlayerData(player);
+    }
+}
